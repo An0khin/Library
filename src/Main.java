3ac0f176ea27cf1cdc81c4321830f9e049009343
Mikhail <anohin04@mail.ru>
2022-06-10 21:14:00 +0500

    Created .jar file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-import java.net.URLEncoder;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import com.home.model.BookList;
 import com.home.view.LibraryWindow;
 import com.home.viewmodel.Library;
@@ -15,9 +8,7 @@ public class Main {
 		LibraryWindow window = new LibraryWindow(library);
 		
 		BookList books = new BookList();
-//		books.addBook(new com.home.model.Book(0, "Hello", "auth", com.home.model.Genres.NULL, 0, "", 5));
-//		books.addBook(new com.home.model.Book(1, "er", "ty", com.home.model.Genres.NULL, 0, "", 5));
-				
+
 		library.setModel(books);
 	}
 }
